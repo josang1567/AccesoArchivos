8634cc73951fa503c8f9c1b74162d25a35ab0660
DavidSM24 <david.dam.4.1.96@gmail.com>
2021-10-04 14:17:12 +0200

    update de app.java para iniciar modal

--- src/main/java/AccesoArchivos/AccesoArchivos/App.java
+++ src/main/java/AccesoArchivos/AccesoArchivos/App.java
@@ -4,32 +4,39 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import models.P_Attack.ExtraDAO;
+import models.P_Character.RolDAO;
+import models.P_User.UserDAO;
+import utils.Conexion;
 
 import java.io.IOException;
+
+import P_Game.SesionDAO;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static Scene scene;
+public void start(Stage stage) throws IOException {
+		
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("primary.fxml"));
+		Parent root = loader.load();
+		Scene scene= new Scene(root);
+		Stage stage2= new Stage();
+		stage2.setScene(scene);
+		//Image image= new Image("file:src/main/resources/images/icons/icon_app.jpg");
+		//stage2.getIcons().add(image);
+		stage2.setTitle("Autenticar Usuario");
+		stage2.setResizable(false);;
+		stage2.initModality(Modality.APPLICATION_MODAL);
+		
+		stage2.show();
+	};
 
     public static void main(String[] args) {
         launch();
