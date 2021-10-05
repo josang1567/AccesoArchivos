e0a500726ee7eb78b3dc6388dcee007f5563c602
joseangelmarinmoreno <60347715+josang1567@users.noreply.github.com>
2021-10-05 08:59:55 +0200

    .

--- src/main/java/AccesoArchivos/models/message_folder/MessageList.java
+++ src/main/java/AccesoArchivos/models/message_folder/MessageList.java
@@ -1,5 +1,33 @@
 package models.message_folder;
 
-public class MessageList {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name="MessageList")
+
+public class MessageList implements Serializable{
+	@XmlElement(name="Messages",type=Message.class)
+	private List<Message> Messages= new ArrayList();
+
+	public MessageList() {}
+
+	public List<Message> getMessages() {
+		return Messages;
+	}
+
+	public void setMessages(List<Message> messages) {
+		Messages = messages;
+	}
+	 
+	public void addMessages(Message newMessage) {
+		this.Messages.add(newMessage);
+	}
+	
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module AccesoArchivos.AccesoArchivos {
     requires javafx.controls;
     requires javafx.fxml;
+	requires java.xml.bind;
 
     opens AccesoArchivos.AccesoArchivos to javafx.fxml;
     exports AccesoArchivos.AccesoArchivos;
