6a191b57f526ffe62617fff9640cb9608dbad8e0
DavidSM24 <david.dam.4.1.96@gmail.com>
2021-10-05 14:12:29 +0200

    update

--- src/main/java/AccesoArchivos/AccesoArchivos/PrimaryController.java
+++ src/main/java/AccesoArchivos/AccesoArchivos/PrimaryController.java
@@ -71,4 +71,5 @@ public class PrimaryController { //login
 		});
 		stage2.show();
 	}
+
 }
--- src/main/java/AccesoArchivos/AccesoArchivos/Room_Selector_Controller.java
+++ src/main/java/AccesoArchivos/AccesoArchivos/Room_Selector_Controller.java
@@ -1,5 +1,7 @@
 package AccesoArchivos.AccesoArchivos;
 
+import java.io.File;
+
 import AccesoArchivos.models.room_folder.Room;
 import AccesoArchivos.models.user_folder.User;
 import javafx.beans.property.SimpleStringProperty;
@@ -9,13 +11,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 public class Room_Selector_Controller {
 	
 	ObservableList<Room> rooms;
-	private User user;		
+	private User user;	
+	private Room room;
 	
 	@FXML
 	protected Pane options_Pane;
@@ -83,4 +87,72 @@ public class Room_Selector_Controller {
     private void changeColorDefaultUserName() {
     	btn_user.setTextFill(Color.BLACK);
     }
+
+    @FXML
+	public void select_Room() { //actualizar esto cuando tenga tiempo por una variable power
+		
+		if(rooms.size()>0) {
+			if(table_room.getSelectionModel().getSelectedItem()!=null) {
+				room= this.table_room.getSelectionModel().getSelectedItem();;
+			}
+//			else {
+//				room=null;
+//				btn_delete_1.setDisable(true);
+//				btn_edit_1.setDisable(true);
+//			}
+
+			if(room!=null) {//setea los valores del chara
+				btn_delete_1.setDisable(false);
+				btn_edit_1.setDisable(false);
+				lab_name.setText(c.getName());
+				lab_universe.setText(c.getUniverse());
+				lab_band.setText(c.getBand());
+				lab_rol.setText(c.getRol().getName());
+				lab_power.setText(
+						(c.getHp()
+						+c.getAtk()
+						+c.getDef()
+						+c.getSpe())+"");
+				
+				File f=new File("file:"+c.getPhoto_face());
+				Image img=new Image(f.getPath());
+				img_view_chara.setImage(img);		
+				
+				if(!c.getOst().matches("no_resource")) {
+					ost=new File(c.getOst());
+					
+					if(!running) {
+						btn_play.setDisable(false);
+					}
+				}
+				else {
+					btn_play.setDisable(true);
+				}				
+			}
+			else {// no hay seleccion --> todo a ""
+				lab_name.setText("");
+				lab_universe.setText("");
+				lab_band.setText("");
+				lab_rol.setText("");
+				lab_power.setText("");
+				img_view_chara.setImage(null);
+				ost=null;
+			}
+		}
+		else { //no hay charas
+			c=null;
+			lab_name.setText("");
+			lab_universe.setText("");
+			lab_band.setText("");
+			lab_rol.setText("");
+			lab_power.setText("");
+			img_view_chara.setImage(null);
+			ost=null;
+			btn_edit_1.setDisable(true);
+			btn_delete_1.setDisable(true);
+			btn_play.setDisable(true);
+		}
+		
+	}
+
 }
