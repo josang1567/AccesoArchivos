a0bc15fb879927b3a534fbecb32a36d3e364b8f0
joseangelmarinmoreno <60347715+josang1567@users.noreply.github.com>
2021-10-08 10:23:36 +0200

    psp

--- src/main/java/AccesoArchivos/AccesoArchivos/PrimaryController.java
+++ src/main/java/AccesoArchivos/AccesoArchivos/PrimaryController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import AccesoArchivos.AccesoArchivos.models.user_folder.User;
+import AccesoArchivos.AccesoArchivos.models.user_folder.UserList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,61 +18,70 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class PrimaryController { //login
+	UserList userl= UserList.getMiRepositorioU(null);
 
+	
 	@FXML
 	protected Button btn_login;
-	
+	@FXML
+    protected TextField name;
+    @FXML
+    protected TextField password;
 	@FXML
 	private void login() throws IOException {
 
-		
-		
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("room_selector.fxml"));
-		Parent root = loader.load();
-		Scene scene= new Scene(root);
-		Room_Selector_Controller room_selector= loader.getController();
-		room_selector.setController(new User(1,"prueba","",true));
-		Stage stage2= new Stage();
-		stage2.setScene(scene);
-		Image image= new Image("file:src/main/resources/images/icons/icon_app.jpg");
-		stage2.getIcons().add(image);
-		stage2.setTitle("Chat XML");
-		stage2.setResizable(false);;
-		stage2.initModality(Modality.WINDOW_MODAL);
-		
-		Stage stage = (Stage) this.btn_login.getScene().getWindow();
-		stage.close();
-		
-		stage2.show();
-		
-		
-		stage2.setOnCloseRequest(new EventHandler<WindowEvent>() {
-			@Override
-			public void handle(WindowEvent e) {				
+		for (int i = 0; i <userl.getUsers().size() ; i++) {
+			if(name.getText().equals(userl.getUsers().get(i).getName()) && password.getText().equals(userl.getUsers().get(i).getPassword()) ) {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("room_selector.fxml"));
+				Parent root = loader.load();
+				Scene scene= new Scene(root);
+				Room_Selector_Controller room_selector= loader.getController();
+				room_selector.setController(new User(1,"prueba","",true));
+				Stage stage2= new Stage();
+				stage2.setScene(scene);
+				Image image= new Image("file:src/main/resources/images/icons/icon_app.jpg");
+				stage2.getIcons().add(image);
+				stage2.setTitle("Chat XML");
+				stage2.setResizable(false);;
+				stage2.initModality(Modality.WINDOW_MODAL);
 				
-				try {			
-					
-					FXMLLoader loader = new FXMLLoader(getClass().getResource("primary.fxml"));
-					Parent root;
-					root = loader.load();
-					Scene scene = new Scene(root);
-					Stage stage2 = new Stage();
-					stage2.setScene(scene);
-					Image image = new Image("file:src/main/resources/images/icons/icon_app.jpg");
-					stage2.getIcons().add(image);
-					stage2.setTitle("Chat XML");
-					stage2.setResizable(false);
-					stage2.initModality(Modality.APPLICATION_MODAL);
-					stage2.show();
-					System.out.println("se ha cerrado el usuario");
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+				Stage stage = (Stage) this.btn_login.getScene().getWindow();
+				stage.close();
 				
+				stage2.show();
+				
+				
+				stage2.setOnCloseRequest(new EventHandler<WindowEvent>() {
+					@Override
+					public void handle(WindowEvent e) {				
+						
+						try {			
+							
+							FXMLLoader loader = new FXMLLoader(getClass().getResource("primary.fxml"));
+							Parent root;
+							root = loader.load();
+							Scene scene = new Scene(root);
+							Stage stage2 = new Stage();
+							stage2.setScene(scene);
+							Image image = new Image("file:src/main/resources/images/icons/icon_app.jpg");
+							stage2.getIcons().add(image);
+							stage2.setTitle("Chat XML");
+							stage2.setResizable(false);
+							stage2.initModality(Modality.APPLICATION_MODAL);
+							stage2.show();
+							System.out.println("se ha cerrado el usuario");
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						
+					}
+				});
+				stage2.show();
 			}
-		});
-		stage2.show();
+		}
+		
+		
 	}
 
 }
