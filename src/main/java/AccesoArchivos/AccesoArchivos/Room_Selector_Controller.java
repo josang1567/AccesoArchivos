3f775d7f329a77b6c32d77eedae0f036a5625490
DavidSM24 <david.dam.4.1.96@gmail.com>
2021-10-13 18:36:23 +0200

    100 % delete user and close session implemented

--- src/main/java/AccesoArchivos/AccesoArchivos/Room_Selector_Controller.java
+++ src/main/java/AccesoArchivos/AccesoArchivos/Room_Selector_Controller.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.xml.bind.JAXBException;
 
 import AccesoArchivos.AccesoArchivos.models.message_folder.Message;
+import AccesoArchivos.AccesoArchivos.models.message_folder.MessageList;
 import AccesoArchivos.AccesoArchivos.models.room_folder.Room;
 import AccesoArchivos.AccesoArchivos.models.room_folder.RoomList;
 import AccesoArchivos.AccesoArchivos.models.user_folder.User;
@@ -21,9 +23,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -43,6 +48,10 @@ public class Room_Selector_Controller {
 	protected Button btn_user;
 	@FXML
 	protected Button btn_enter;
+	@FXML
+	protected Button btn_close_sesion;
+	@FXML
+	protected Button btn_delete_user;
 	@FXML
 	protected TableView<Room> table_room;
 	@FXML
@@ -78,66 +87,162 @@ public class Room_Selector_Controller {
 	}
 
 	public void enterRoom() {
-		
+
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("chat_room.fxml"));
 			Parent root;
 			root = loader.load();
-			Scene scene= new Scene(root);
-			Chat_Room_Controller chat_room=loader.getController();
+			Scene scene = new Scene(root);
+			Chat_Room_Controller chat_room = loader.getController();
 			room.getLog_users().add(user);
-			RoomList rl=RoomList.getMiRepositorioM();
+			RoomList rl = RoomList.getMiRepositorioM();
 			rl.save();
-			chat_room.setController(user,room);
-			Stage stage2= new Stage();
+			chat_room.setController(user, room);
+			Stage stage2 = new Stage();
 			stage2.setScene(scene);
-			Image image= new Image("file:src/main/resources/images/icons/icon_app.jpg");
+			Image image = new Image("file:src/main/resources/images/icons/icon_app.jpg");
 			stage2.getIcons().add(image);
-			stage2.setTitle("Chat: "+room.getName());
-			stage2.setResizable(false);;
+			stage2.setTitle("Chat: " + room.getName());
+			stage2.setResizable(false);
+			;
 			stage2.initModality(Modality.WINDOW_MODAL);
-			
+
 			Stage stage = (Stage) this.btn_enter.getScene().getWindow();
 			stage.close();
 
 			stage2.show();
 			stage2.setOnCloseRequest(new EventHandler<WindowEvent>() {
 				@Override
-				public void handle(WindowEvent e) {				
-					
-					try {			
+				public void handle(WindowEvent e) {
+
+					try {
 						user.setOnline(false);
-						UserList ul=UserList.getMiRepositorioU();
+						UserList ul = UserList.getMiRepositorioU();
 						ul.reeplaceUser(user);
 						ul.save();
 						chat_room.t.cancel();
-						
-						if(room.getLog_users().size()>0) {
-							if(room.getLog_users().size()==1) {
+
+						if (room.getLog_users().size() > 0) {
+							if (room.getLog_users().size() == 1) {
 								room.getLog_users().remove(0);
-							}
-							else {
-								int i=room.getLog_users().lastIndexOf(user);
+							} else {
+								int i = room.getLog_users().lastIndexOf(user);
 								room.getLog_users().remove(i);
 							}
 							rl.reeplaceRoom(room);
 							rl.save();
 						}
-						
-					}
-					catch (Exception e2) {
+
+					} catch (Exception e2) {
 						// TODO: handle exception
-					}	
+					}
 				}
 			});
-			
+
 		} catch (IOException | JAXBException e2) {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
 		}
+
+	}
+
+	@FXML
+	public void close_session() throws IOException, JAXBException {
+		close_Options_Pane();
+
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setHeaderText(null);
+		alert.setTitle("Confirmación");
+		alert.setContentText("¿Se cerrará su sesión, desea continuar?");
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK) {
+			user.setOnline(false);
+			UserList ul = UserList.getMiRepositorioU();
+			ul.reeplaceUser(user);
+			ul.save();
+
+			Stage stage = (Stage) this.btn_close_sesion.getScene().getWindow();
+			stage.close();
+
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("primary.fxml"));
+			Parent root = loader.load();
+			Scene scene = new Scene(root);
+			PrimaryController primary = loader.getController();
+			Stage stage2 = new Stage();
+			stage2.setScene(scene);
+			Image image = new Image("file:src/main/resources/images/icons/icon_app.jpg");
+			stage2.getIcons().add(image);
+			stage2.setTitle("Inicio de Sesión");
+			stage2.setResizable(false);
+			;
+			stage2.initModality(Modality.APPLICATION_MODAL);
+
+			stage2.show();
+		}
+	}
+
+	public void delete_user() throws IOException, JAXBException {
+		close_Options_Pane();
 		
-		
-		
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setHeaderText(null);
+		alert.setTitle("Confirmación");
+		alert.setContentText("¿Está seguro de que quiere borrar su usuario?");
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK) {
+			user.setOnline(false);
+			user.setName(user.getName()+"\n(DELETED ACCOUNT)");
+			user.setPassword("");
+			UserList ul = UserList.getMiRepositorioU();
+			ul.reeplaceUser(user);
+			ul.save();
+			MessageList ml=MessageList.getMiRepositorioM();
+			ml.charge();
+			if(ml.Messages!=null&&ml.Messages.size()>0){
+				for(Message m:ml.Messages) {
+					if(m.getUser().getId()==user.getId()) {
+						m.setUser(user);
+					}
+				}
+			}
+			ml.save();
+			RoomList rl=RoomList.getMiRepositorioM();
+			rl.charge();
+			for(Room r:rl.getRooms()) {
+				if(r.getMessages()!=null&&r.getMessages().size()>0) {
+					for(Message m2:r.getMessages()) {
+						if(m2.getUser().getId()==user.getId()) {
+							m2.setUser(user);
+						}
+					}
+				}
+			}
+			rl.save();
+			
+			Alert alert2=new Alert(AlertType.INFORMATION);
+    		alert.setHeaderText(null);
+    		alert.setTitle("Información");
+    		alert.setContentText("¡Usuario eliminado correctamente!");
+    		alert.showAndWait();
+    		
+    		Stage stage = (Stage) this.btn_close_sesion.getScene().getWindow();
+			stage.close();
+
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("primary.fxml"));
+			Parent root = loader.load();
+			Scene scene = new Scene(root);
+			PrimaryController primary = loader.getController();
+			Stage stage2 = new Stage();
+			stage2.setScene(scene);
+			Image image = new Image("file:src/main/resources/images/icons/icon_app.jpg");
+			stage2.getIcons().add(image);
+			stage2.setTitle("Inicio de Sesión");
+			stage2.setResizable(false);
+			;
+			stage2.initModality(Modality.APPLICATION_MODAL);
+
+			stage2.show();
+		}
 	}
 
 	@FXML
@@ -148,11 +253,33 @@ public class Room_Selector_Controller {
 	@FXML
 	private void close_Options_Pane() {
 		options_Pane.setVisible(false);
+		btn_delete_user.setTextFill(Color.BLACK);
+		btn_close_sesion.setTextFill(Color.BLACK);
 	}
 
 	@FXML
 	private void changeColorUserName() {
 		btn_user.setTextFill(Color.WHITE);
+	}
+
+	@FXML
+	private void Default_close_session() {
+		btn_close_sesion.setTextFill(Color.BLACK);
+	}
+
+	@FXML
+	private void Default_delete_user() {
+		btn_delete_user.setTextFill(Color.BLACK);
+	}
+
+	@FXML
+	private void changeColor_close_Session() {
+		btn_close_sesion.setTextFill(Color.RED);
+	}
+
+	@FXML
+	private void changeColor_delete_user() {
+		btn_delete_user.setTextFill(Color.RED);
 	}
 
 	@FXML
